9d4c42018b317e2e36305d11cf6eae78371f667b
Jonathan Vale <creativeweb.de@gmail.com>
2024-07-07 19:24:24 -0300

    switch koa-graphql to graphql-http

--- src/server/app/app.ts
+++ src/server/app/app.ts
@@ -1,33 +1,18 @@
 import Koa from 'koa';
 import logger from 'koa-logger';
-import bodyParser from 'koa-bodyparser';
 import Router from 'koa-router';
-import { graphqlHTTP } from 'koa-graphql';
+import mount from 'koa-mount';
+import { createHandler } from 'graphql-http/lib/use/koa';
 import { schema } from './graph-ql/schema';
 
 const app = new Koa();
 const routes = new Router();
 
 app.use(logger());
-app.use(bodyParser({
-  onerror(err: Error, ctx: Koa.Context) {
-    ctx.throw(err, 422)
-  },
-}));
 
-routes.all('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: {
-    headerEditorEnabled: true,
-    shouldPersistHeaders: true,
-  },
-  customFormatErrorFn: (error) => {
-    return {
-      message: error.message,
-      locations: error.locations
-    };
-  }
-}));
+app.use(mount('/graphql', createHandler({
+  schema: schema
+})));
 
 app.use(routes.routes());
 app.use(routes.allowedMethods());
